31c098eb16be353e943f1c48145817fa1427b46f
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-06-19 01:43:13 +0530

    Update chat completion request to enhance sentiment analysis functionality and improve response format

--- app.js
+++ app.js
@@ -4,21 +4,34 @@ const client = new OpenAI({
     baseURL: "https://api.groq.com/openai/v1",
 });
 
+// zod, schema validation library, can be used to validate 
+
 const response = await client.chat.completions.create({
     temperature: 1,
     top_p: 1, // set temperature or top_p, but not both
-    stop: '',
+    stop: '', // stop sequence for the model to stop generating further tokens
+    max_completion_tokens:  1000, // to limit the number of tokens in output 
+    frequency_penalty: 1, // to penalize new tokens based on their existing frequency in the text so far
+    presence_penalty: 1,
     model: "openai/gpt-oss-20b",
+    response_format: { 'type': 'json_object'},
+    
     messages: [
         {
             role: 'system',
-            content: 'You are a jarvis, a smart personal assistant. Be alway polite and helpful.'
+            content: `You are a jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. 
+            Classify the review as positve, Netural or negative. You must return the result in Valid JSON Sturcture.
+            example: {"sentiment" : "Negative"} 
+            `
         },
         {
             role: 'user',
-            content: "who you are"
+            content: `Review: These headphones arrived quickly and look great, but the left earcup stop working after a week.
+            Sentiment:
+            ` 
         }
     ],
 });
-console.log(response.choices[0].message.content);
+// console.log(response.choices[0].message.content);
+console.log(JSON.parse(response?.choices[0]?.message?.content));
 
