e56535f471ef90d8730ca6711b2c38d35762d473
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-09-22 00:44:32 +0530

    Add a newline at the end of app.js for better file formatting

--- app.js
+++ app.js
@@ -106,3 +106,4 @@ const response = await client.chat.completions.create({
 // console.log(response.choices[0].message.content);
 console.log(JSON.parse(response?.choices[0]?.message?.content));
 
+
